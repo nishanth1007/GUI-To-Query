a6796475795a9de8975c7483a3e7272c31ca09a0
Nishanth Singamsetty <nikiamcool@gmail.com>
2019-09-09 03:24:16 -0500

    Add files via upload
    A preliminary version of a cloud-based database application where a webpage GUI allowing users to insert, modify and query the database.

---
+++ WebForm1.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication9.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <div style=" background-color: #EFF3FB; border: double;  margin-left:auto; margin-right:auto; letter-spacing: normal; text-indent: inherit; vertical-align: middle; cursor: crosshair;">
+            <div style="padding-left:30%"><br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label" Font-Size="X-Large"></asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" Height="58px" Width="448px" BackColor="#FFFFCC" BorderColor="Black" BorderStyle="Double" BorderWidth="3px" Font-Bold="True" Font-Names="Arial Rounded MT Bold" padding-left="20px" Font-Overline="False" Font-Size="Large" Font-Underline="False" ForeColor="Black" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Submit" background-color=" #4CAF50" color="white"  text-align="center" font-size="XX-Large" padding="6px" BackColor="#4CAF50" BorderWidth="3px" Font-Names="Arial Rounded MT Bold" BorderStyle="Double" ForeColor="White" />
+    &nbsp;&nbsp;
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br /></div>
+            <div style=" background-color: #EFF3FB; text-align:center; margin-left:auto; margin-right:auto; padding-left:30%; letter-spacing: normal; text-indent: inherit; vertical-align: middle; cursor: crosshair;"">
+                <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" >
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+            </asp:GridView></div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text;
+using System.Data;
+
+namespace WebApplication9
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public string connectionString;
+        public SqlConnection sqlConnection;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "Please enter your SQL query";
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            connectionString = @"Data Source=karansqlserver.database.windows.net;Initial Catalog=IDS520SP19 ;User ID=karanuser;Password=.superman1914";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+                
+                // Response.Write("Connection Made");
+
+            /*          //SqlCommand cmd = new SqlCommand("select firstName from dbo.customer where password='Wb4~RnVU';", sqlConnection);
+                     // SqlDataReader dr = cmd.ExecuteReader();
+                      StringBuilder sb = new StringBuilder();
+                      sb.Append("select firstName from dbo.customer");
+
+                      String sql = sb.ToString();
+
+                      DataTable dt = new DataTable(); */
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(TextBox1.Text);
+
+            String sql = sb.ToString();
+            using (SqlCommand cmd = new SqlCommand(sql, sqlConnection))
+            {
+                try
+                {
+                    //Response.Write(cmd.ToString());
+                    SqlDataAdapter sqlda = new SqlDataAdapter(sql, sqlConnection);
+                    DataTable dtb1 = new DataTable();
+                    sqlda.Fill(dtb1);
+                    GridView1.DataSource = dtb1;
+                    GridView1.DataBind();
+                    /* using (SqlDataReader reader = cmd.ExecuteReader())
+                     {
+                         //Response.Write(cmd.ToString());
+                         while (reader.Read())
+                         {
+                             Response.Write(reader.GetString(0));
+
+                         }
+                     } */
+
+                }
+                catch
+                {
+                    //Response.Write("Please enter a valid sql query");
+                    Label1.Text = "Please enter a valid sql query";
+
+                }
+
+                finally
+                {
+                    sqlConnection.Close();
+
+                }
+            }
+            }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
